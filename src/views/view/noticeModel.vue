bd054bd6b468b3c4904d91b7ab064b4c981ca5f7
wuna <wuna@utry.cn>
2017-07-26 17:20:18 +0800

    doc -- noticeModel Signed-off-by: wuna <wuna@utry.cn>

--- src/views/view/noticeModel.vue
+++ src/views/view/noticeModel.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
+  <article class="doc-alert-container">
+    <h1>NoticeModel 通知提醒</h1>
+    <h2>概述</h2>
+    <p>
+      在指定dom上的全局通知，常用于以下场景：
+      <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;通知内容带有描述信息
+      <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;系统主动推送
+    </p>
+    <h2>示例代码</h2>
+    <section class="demo">
     <demoTab :code="noticeModelCode" :describeTitle="describeTitle">
       <div slot="sample">
           <div class="noticeHeight">
@@ -8,11 +17,12 @@
           </div>
       </div>
       <div slot="describe-content">
-        常用于以下场景：
-        通知内容带有描述信息
+       <p> 一般用于dom的点击事件上,比如button的click事件。</p>
+       <p> 根据参数 width height 设置noticeModel的大小,根据isShow来控制noticeModel的显示隐藏。</p>
       </div>
     </demoTab>
-  </div>
+    </section>
+  </article>
 </template>
 <script>
   import demoTab from '@/components/DemoTab'
