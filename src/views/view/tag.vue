256a9e30bcd1ab28d204ef45c721ef66ab3f42f4
yanyingying <yanyingying@utry.cn>
2017-07-27 14:41:33 +0800

    tag，toolTip

--- src/views/view/tag.vue
+++ src/views/view/tag.vue
@@ -1,23 +1,75 @@
 <template>
   <article class="doc-tag-container">
-    <h2>Tag标签</h2>
-    <h3>概述</h3>
+    <h1>Tag标签</h1>
+    <h2>概述</h2>
     <p>
-      对不同维度进行简单的标记和分类。对不同维度进行简单的标记和分类。对不同维度进行简单的标记和分类。对不同维度进行简单的标记和分类。对不同维度进行简单的标记和分类。对不同维度进行简单的标记和分类。对不同维度进行简单的标记和分类。对不同维度进行简单的标记和分类。
+      对不同维度进行简单的标记和分类。
     </p>
-    <h3>示例代码</h3>
-    <demoTab :code="gridCode" :describeTitle="subTitle1">
+    <h2>代码示例</h2>
+    <section class="demo">
+    <demoTab :code="tagCode" :describeTitle="subTitle1">
       <div slot="sample">
          <Tag>tag1</Tag>
          <Tag>tag2</Tag>
          <Tag>tag3</Tag>
-         <Tag closable>tag4</Tag>
+         <Tag v-if="show" closable @on-close="handleClose">tag4</Tag>
       </div>
       <div slot="describe-content">
-         简单的展示，添加属性closable可以关闭标签。<br/>
+        简单的展示，添加属性<Tag color="grey">closable</Tag>可以关闭标签。<br/>
          点击关闭标签时，会触发 on-close 事件，需自己实现关闭逻辑。
       </div>
     </demoTab>
+    </section>
+
+    <section class="demo">
+      <demoTab :code="tagCode2" :describeTitle="subTitle2">
+        <div slot="sample">
+          <Tag type="border">标签三</Tag>
+          <Tag type="border" closable>标签四</Tag>
+          <Tag type="dot">标签一</Tag>
+          <Tag type="dot" closable>标签二</Tag>
+        </div>
+        <div slot="describe-content">
+          通过设置<Tag color="grey">type</Tag>属性为<Tag color="grey">border</Tag>或<Tag color="grey">dot</Tag>来选择不同的样式类型。建议有关闭的某些场景下使用border，图例的场景下使用dot。
+        </div>
+      </demoTab>
+    </section>
+
+    <section class="demo">
+      <demoTab :code="tagCode3" :describeTitle="subTitle3">
+        <div slot="sample">
+          <Tag closable color="blue">标签一</Tag>
+          <Tag closable color="green">标签二</Tag>
+          <Tag closable color="red">标签三</Tag>
+          <Tag closable color="yellow">标签四</Tag>
+          <br><br>
+          <Tag type="border" closable color="blue">标签一</Tag>
+          <Tag type="border" closable color="green">标签二</Tag>
+          <Tag type="border" closable color="red">标签三</Tag>
+          <Tag type="border" closable color="yellow">标签四</Tag>
+          <br><br>
+          <Tag type="dot" closable color="blue">标签一</Tag>
+          <Tag type="dot" closable color="green">标签二</Tag>
+          <Tag type="dot" closable color="red">标签三</Tag>
+          <Tag type="dot" closable color="yellow">标签四</Tag>
+        </div>
+        <div slot="describe-content">
+          四种不同的颜色。
+        </div>
+      </demoTab>
+    </section>
+
+    <section class="demo">
+      <demoTab :code="tagCode4" :describeTitle="subTitle4">
+        <div slot="sample">
+          <Tag v-for="item in count" :key="item" :name="item" closable @on-close="handleClose2">标签{{ item + 1 }}</Tag>
+          <Button icon="ios-plus-empty" type="dashed" size="small" @click="handleAdd">添加标签</Button>
+        </div>
+        <div slot="describe-content">
+          用数组生成一组标签，可以动态添加和删除。
+        </div>
+      </demoTab>
+    </section>
   </article>
 </template>
 <script>
@@ -29,29 +81,134 @@ export default {
   data () {
     return {
       "subTitle1": "基本用法",
-      "gridCode": `
-&lt;template>
-   &lt;Tag>tag1&lt;/Tag>
-   &lt;Tag>tag2&lt;/Tag>
-   &lt;Tag>tag3&lt;/Tag>
-   &lt;Tag closable>tag4&lt;/Tag>
-&lt;/template>
-&lt;script>
-    export default {
-        
+      "tagCode": `
+        &lt;template>
+           &lt;Tag>tag1&lt;/Tag>
+           &lt;Tag>tag2&lt;/Tag>
+           &lt;Tag>tag3&lt;/Tag>
+           &lt;Tag v-if="show" closable @on-close="handleClose">tag4&lt;/Tag>
+        &lt;/template>
+        &lt;script>
+            export default {
+              data () {
+                return {
+                    show: true
+                }
+              },
+              methods: {
+                  handleClose () {
+                      this.show = false;
+                  }
+              }
+            }
+        &lt;/script>
+        `,
+      "subTitle2": "样式类型",
+      "tagCode2": `
+        &lt;template>
+           &lt;Tag type="border">标签三&lt;/Tag>
+           &lt;Tag type="border" closable>标签四&lt;/Tag>
+           &lt;Tag type="dot">标签一&lt;/Tag>
+           &lt;Tag type="dot" closable>标签二&lt;/Tag>
+        &lt;/template>
+        &lt;script>
+            export default {
+
+            }
+        &lt;/script>
+        `,
+      "subTitle3": "各种颜色",
+      "tagCode3": `
+        &lt;template>
+           &lt;Tag closable color="blue">标签一&lt;/Tag>
+           &lt;Tag closable color="green">标签二&lt;/Tag>
+           &lt;Tag closable color="red">标签三&lt;/Tag>
+           &lt;Tag closable color="yellow">标签四&lt;/Tag>
+           &lt;br>&lt;br>
+           &lt;Tag type="border" closable color="blue">标签一&lt;/Tag>
+           &lt;Tag type="border" closable color="green">标签二&lt;/Tag>
+           &lt;Tag type="border" closable color="red">标签三&lt;/Tag>
+           &lt;Tag type="border" closable color="yellow">标签四&lt;/Tag>
+           &lt;br>&lt;br>
+           &lt;Tag type="dot" closable color="blue">标签一&lt;/Tag>
+           &lt;Tag type="dot" closable color="green">标签二&lt;/Tag>
+           &lt;Tag type="dot" closable color="red">标签三&lt;/Tag>
+           &lt;Tag type="dot" closable color="yellow">标签四&lt;/Tag>
+        &lt;/template>
+        &lt;script>
+            export default {
+
+            }
+        &lt;/script>
+        `,
+      "subTitle4": "动态添加和删除",
+      "tagCode4": `
+        &lt;template>
+           &lt;Tag v-for="item in count" :key="item" :name="item" closable @on-close="handleClose2">标签{{ item + 1 }}&lt;/Tag>
+           &lt;Button icon="ios-plus-empty" type="dashed" size="small" @click="handleAdd">添加标签&lt;/Button>
+        &lt;/template>
+        &lt;script>
+            export default {
+              data () {
+                return {
+                    count: [0, 1, 2]
+                }
+              },
+              methods: {
+                  handleAdd () {
+                      if (this.count.length) {
+                          this.count.push(this.count[this.count.length - 1] + 1);
+                      } else {
+                          this.count.push(0);
+                      }
+                  },
+                  handleClose2 (event, name) {
+                      const index = this.count.indexOf(name);
+                      this.count.splice(index, 1);
+                  }
+              }
+            }
+        &lt;/script>
+        `,
+      count: [0, 1, 2],
+      show: true
     }
-&lt;/script>
-`
+  },
+  methods:{
+    handleClose(){
+      this.show = false;
+    },
+    handleAdd () {
+      if (this.count.length) {
+        this.count.push(this.count[this.count.length - 1] + 1);
+      } else {
+        this.count.push(0);
+      }
+    },
+    handleClose2 (event, name) {
+      const index = this.count.indexOf(name);
+      this.count.splice(index, 1);
     }
   }
 }
 </script>
 <style lang="less">
-article {
-  padding-left: 30px;
-  h2{
-    margin-top:45px;
-    line-height:40px;
+  article {
+    padding-left: 30px;
+    font-size: 14px;
+    h1, h2, h3{
+      margin:12px 0px;
+      font-weight: normal;
+    }
+    h1{
+      margin-top: 40px;
+      font-size: 26px;
+    }
+    h2{
+      font-size:20px;
+    }
+    section.demo{
+      margin: 20px 0px;
+    }
   }
-}
 </style>
--- src/views/view/toolTip.vue
+++ src/views/view/toolTip.vue
@@ -1,11 +1,296 @@
 <template>
-  <div class="">
-    toolTip
-  </div>
+  <article class="doc-toolTip-container">
+    <h1>Tooltip 文字提示</h1>
+    <h2>概述</h2>
+    <p>
+      文字提示气泡框，在鼠标悬停时显示，代替了系统的<Tag color="grey">title</Tag>提示。
+    </p>
+    <h2>代码示例</h2>
+  <section class="demo">
+    <demoTab :code="toolTipCode" :describeTitle="describeTitle">
+      <div slot="sample">
+        <Tooltip content="这里是提示文字">
+          当鼠标经过这段文字时，会显示一个气泡框
+        </Tooltip>
+      </div>
+      <div slot="describe-content">
+        最简单的用法。
+
+        注意 Tooltip 内的文本使用了<Tag color="grey">white-space: nowrap;</Tag>，即不自动换行，如需展示很多内容并自动换行时，建议给内容 slot 增加样式<Tag color="grey">white-space: normal;</Tag>。
+      </div>
+    </demoTab>
+  </section>
+
+  <section class="demo">
+    <demoTab :code="toolTipCode2" :describeTitle="describeTitle2">
+      <div slot="sample">
+        <Tooltip placement="top" content="Tooltip 文字提示" :delay="1000">
+          <Button @click="disabled = true">延时1秒显示</Button>
+        </Tooltip>
+      </div>
+      <div slot="describe-content">
+        通过设置属性<Tag color="grey">delay</Tag>可以延时显示文字提示，单位毫秒。
+      </div>
+    </demoTab>
+  </section>
+
+  <section class="demo">
+    <demoTab :code="toolTipCode3" :describeTitle="describeTitle3">
+      <div slot="sample">
+        <div class="top">
+          <Tooltip content="Top Left 文字提示" placement="top-start">
+            <Button>上左</Button>
+          </Tooltip>
+          <Tooltip content="Top Center 文字提示" placement="top">
+            <Button>上边</Button>
+          </Tooltip>
+          <Tooltip content="Top Right 文字提示" placement="top-end">
+            <Button>上右</Button>
+          </Tooltip>
+        </div>
+        <div class="center">
+          <div class="center-left">
+            <Tooltip content="Left Top 文字提示" placement="left-start">
+              <Button>左上</Button>
+            </Tooltip><br><br>
+            <Tooltip content="Left Center 文字提示" placement="left">
+              <Button>左边</Button>
+            </Tooltip><br><br>
+            <Tooltip content="Left Bottom 文字提示" placement="left-end">
+              <Button>左下</Button>
+            </Tooltip>
+          </div>
+          <div class="center-right">
+            <Tooltip content="Right Top 文字提示" placement="right-start">
+              <Button>右上</Button>
+            </Tooltip><br><br>
+            <Tooltip content="Right Center 文字提示" placement="right">
+              <Button>右边</Button>
+            </Tooltip><br><br>
+            <Tooltip content="Right Bottom 文字提示" placement="right-end">
+              <Button>右下</Button>
+            </Tooltip>
+          </div>
+        </div>
+        <div class="bottom">
+          <Tooltip content="Bottom Left 文字提示" placement="bottom-start">
+            <Button>下左</Button>
+          </Tooltip>
+          <Tooltip content="Bottom Center 文字提示" placement="bottom">
+            <Button>下边</Button>
+          </Tooltip>
+          <Tooltip content="Bottom Right 文字提示" placement="bottom-end">
+            <Button>下右</Button>
+          </Tooltip>
+        </div>
+      </div>
+      <div slot="describe-content">
+        组件提供了12个不同的方向显示Tooltip，具体配置可查看API。
+      </div>
+    </demoTab>
+  </section>
+
+  <section class="demo">
+    <demoTab :code="toolTipCode4" :describeTitle="describeTitle4">
+      <div slot="sample">
+        <Tooltip placement="top">
+          <Button>多行</Button>
+          <div slot="content">
+            <p>显示多行信息</p>
+            <p><i>可以自定义样式</i></p>
+          </div>
+        </Tooltip>
+      </div>
+      <div slot="describe-content">
+        通过自定义 slot 显示多行文本或更复杂的样式。
+      </div>
+    </demoTab>
+  </section>
+
+  <section class="demo">
+    <demoTab :code="toolTipCode5" :describeTitle="describeTitle5">
+      <div slot="sample">
+        <Tooltip placement="top" content="可以禁用文字提示" :disabled="disabled">
+          <Button @click="closeTip">点击关闭提示</Button>
+        </Tooltip>
+      </div>
+      <div slot="describe-content">
+        通过设置属性<Tag color="grey">disabled</Tag>可以禁用文字提示。
+      </div>
+    </demoTab>
+  </section>
+  </article>
 </template>
 <script>
+  import demoTab from '@/components/DemoTab'
+  export default {
+    components: {
+      demoTab
+    },
+    data () {
+      return {
+        disabled: false,
+        "describeTitle": "基础用法",
+        "toolTipCode": `
+          &lt;template>
+            &lt;Tooltip content="这里是提示文字">
+              当鼠标经过这段文字时，会显示一个气泡框
+            &lt;/Tooltip>
+          &lt;/template>
+          &lt;script>
+          export default {
 
+          }
+          &lt;/script>
+        `,
+        "describeTitle2": "延时",
+        "toolTipCode2": `
+          &lt;template>
+            &lt;Tooltip placement="top" content="Tooltip 文字提示" :delay="1000">
+              &lt;Button @click="disabled = true">延时1秒显示&lt;/Button>
+            &lt;/Tooltip>
+          &lt;/template>
+          &lt;script>
+          export default {
+
+          }
+          &lt;/script>
+        `,
+        "describeTitle3": "位置",
+        "toolTipCode3": `
+          &lt;template>
+            &lt;div class="top">
+                &lt;Tooltip content="Top Left 文字提示" placement="top-start">
+                    &lt;Button>上左&lt;/Button>
+                &lt;/Tooltip>
+                &lt;Tooltip content="Top Center 文字提示" placement="top">
+                    &lt;Button>上边&lt;/Button>
+                &lt;/Tooltip>
+                &lt;Tooltip content="Top Right 文字提示" placement="top-end">
+                    &lt;Button>上右&lt;/Button>
+                &lt;/Tooltip>
+            &lt;/div>
+            &lt;div class="center">
+                &lt;div class="center-left">
+                    &lt;Tooltip content="Left Top 文字提示" placement="left-start">
+                        &lt;Button>左上&lt;/Button>
+                    &lt;/Tooltip>&lt;br>&lt;br>
+                    &lt;Tooltip content="Left Center 文字提示" placement="left">
+                        &lt;Button>左边&lt;/Button>
+                    &lt;/Tooltip>&lt;br>&lt;br>
+                    &lt;Tooltip content="Left Bottom 文字提示" placement="left-end">
+                        &lt;Button>左下&lt;/Button>
+                    &lt;/Tooltip>
+                &lt;/div>
+                &lt;div class="center-right">
+                    &lt;Tooltip content="Right Top 文字提示" placement="right-start">
+                        &lt;Button>右上&lt;/Button>
+                    &lt;/Tooltip>&lt;br>&lt;br>
+                    &lt;Tooltip content="Right Center 文字提示" placement="right">
+                        &lt;Button>右边&lt;/Button>
+                    &lt;/Tooltip>&lt;br>&lt;br>
+                    &lt;Tooltip content="Right Bottom 文字提示" placement="right-end">
+                        &lt;Button>右下&lt;/Button>
+                    &lt;/Tooltip>
+                &lt;/div>
+            &lt;/div>
+            &lt;div class="bottom">
+                &lt;Tooltip content="Bottom Left 文字提示" placement="bottom-start">
+                    &lt;Button>下左&lt;/Button>
+                &lt;/Tooltip>
+                &lt;Tooltip content="Bottom Center 文字提示" placement="bottom">
+                    &lt;Button>下边&lt;/Button>
+                &lt;/Tooltip>
+                &lt;Tooltip content="Bottom Right 文字提示" placement="bottom-end">
+                    &lt;Button>下右&lt;/Button>
+                &lt;/Tooltip>
+            &lt;/div>
+          &lt;/template>
+          &lt;script>
+          export default {
+
+          }
+          &lt;/script>
+        `,
+        "describeTitle4": "自定义内容",
+        "toolTipCode4": `
+          &lt;template>
+            &lt;Tooltip placement="top">
+              &lt;Button>多行&lt;/Button>
+              &lt;div slot="content">
+                  &lt;p>显示多行信息&lt;/p>
+                  &lt;p>&lt;i>可以自定义样式&lt;/i>&lt;/p>
+              &lt;/div>
+            &lt;/Tooltip>
+          &lt;/template>
+          &lt;script>
+          export default {
+
+          }
+          &lt;/script>
+        `,
+        "describeTitle5": "禁用",
+        "toolTipCode5": `
+          &lt;template>
+            &lt;Tooltip placement="top" content="可以禁用文字提示" :disabled="disabled">
+              &ltButton @click="disabled = true">点击关闭提示&lt/Button>
+            &lt;/Tooltip>
+          &lt;/template>
+          &lt;script>
+          export default {
+            data () {
+              return {
+                  disabled: false
+              }
+            }
+          }
+          &lt;/script>
+        `
+      }
+    },
+    methods: {
+      closeTip() {
+          if(this.disabled){
+              this.disabled = false;
+          }else{
+              this.disabled = true;
+          }
+
+      }
+    }
+  }
 </script>
-<style>
-
+<style lang="less">
+  article {
+    padding-left: 30px;
+    font-size: 14px;
+    h1, h2, h3{
+      margin:12px 0px;
+      font-weight: normal;
+    }
+    h1{
+      margin-top: 40px;
+      font-size: 26px;
+    }
+    h2{
+      font-size:20px;
+    }
+    section.demo{
+      margin: 20px 0px;
+    }
+  }
+  .top,.bottom{
+    text-align: center;
+  }
+  .center{
+    width: 300px;
+    margin: 10px auto;
+    overflow: hidden;
+  }
+  .center-left{
+    float: left;
+  }
+  .center-right{
+    float: right;
+  }
 </style>
