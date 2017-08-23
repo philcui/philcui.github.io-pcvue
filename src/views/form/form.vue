34060412d33392d57a17cb3579e323f629172c88
renxiaoyi <renxiaoyi@utry.cn>
2017-08-23 15:48:30 +0800

    form.vue

--- src/views/form/form.vue
+++ src/views/form/form.vue
@@ -710,7 +710,68 @@
               &lt;Button type="ghost" @click="handleReset('formValidate')" style="margin-left: 8px">重置&lt;/Button>
             &lt;/Form-item>
           &lt;/Form>
-&lt;/template>`,
+&lt;/template>
+&lt;script>
+export default{
+  data(){
+    return{
+      formValidate: {
+        name: '',
+        mail: '',
+        city: '',
+        gender: '',
+        interest: [],
+        date: '',
+        time: '',
+        desc: ''
+      },
+      ruleValidate: {
+        name: [
+          { required: true, message: '姓名不能为空', trigger: 'blur' }
+        ],
+        mail: [
+          { required: true, message: '邮箱不能为空', trigger: 'blur' },
+          { type: 'email', message: '邮箱格式不正确', trigger: 'blur' }
+        ],
+        city: [
+          { required: true, message: '请选择城市', trigger: 'change' }
+        ],
+        gender: [
+          { required: true, message: '请选择性别', trigger: 'change' }
+        ],
+        interest: [
+          { required: true, type: 'array', min: 1, message: '至少选择一个爱好', trigger: 'change' },
+          { type: 'array', max: 2, message: '最多选择两个爱好', trigger: 'change' }
+        ],
+        date: [
+          { required: true, type: 'date', message: '请选择日期', trigger: 'change' }
+        ],
+        time: [
+          { required: true, type: 'date', message: '请选择时间', trigger: 'change' }
+        ],
+        desc: [
+          { required: true, message: '请输入个人介绍', trigger: 'blur' },
+          { type: 'string', min: 20, message: '介绍不能少于20字', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    handleSubmit (name) {
+      this.$refs[name].validate((valid) => {
+        if (valid) {
+          this.$Message.success('提交成功!');
+        } else {
+          this.$Message.error('表单验证失败!');
+        }
+      })
+    },
+    handleReset (name) {
+      this.$refs[name].resetFields();
+    }
+  }
+}
+&lt;/script>`,
         "inputCode2":`&lt;template>
         &lt;Form :model="formLeft" label-position="left" :label-width="100">
           &lt;Form-item label="标题">
@@ -745,7 +806,20 @@
             &lt;Input v-model="formTop.input3">&lt;/Input>
           &lt;/Form-item>
         &lt;/Form>
-        &lt;/template>`,
+        &lt;/template>
+&lt;script>
+export default{
+  data(){
+    return{
+      formLeft: {
+        input1: '',
+        input2: '',
+        input3: ''
+      }
+    }
+  }
+}
+&lt;/script>`,
         subTitle1:'基础用法',
         "inputCode": `
             &lt;template>
@@ -802,6 +876,22 @@
             &lt;/template>
             &lt;script>
                 export default {
+                  data(){
+                    return{
+                      formItem:{
+                        input:'',
+                        city:'',
+                        date: '',
+                        time: '',
+                        radio: 'male',
+                        checkbox: [],
+                        desc:'',
+                        switch:true,
+                        slider:[20, 50]
+                      },
+                    }
+                  }
+
                 }
             &lt;/script>
             `,
